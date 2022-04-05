a041ed56e46e4926cf6e6d832e4a339c757deab4
Shawn-Fries <samfoley275@gmail.com>
2022-04-05 10:10:53 -0400

    add showtime seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'guestlogin', password: 'guestlogin')
-Movie.create(title: 'Licorice Pizza', genre: 'Comedy', release_date: '2021-11-26 00:00:00', description: 'The story of Alana Kane and Gary Valentine growing up, running around and going through the treacherous navigation of first love in the San Fernando Valley, 1973.')
-Movie.create(title: 'Spider-Man: Into the Spider-Verse', genre: 'Action', release_date: '2018-12-01 00:00:00', description: 'Teen Miles Morales becomes the Spider-Man of his universe, and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.')
-Movie.create(title: 'The Good, the Bad and the Ugly', genre: 'Western', release_date: '1966-12-23 00:00:00', description: 'A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.')
-Review.create(rating: 10, body: 'Profound', author_id: 1, movie_id: 1)
-Review.create(rating: 1, body: 'Pretentious', author_id: 2, movie_id: 1)
+# User.create(username: 'guestlogin', password: 'guestlogin')
+# Movie.create(title: 'Licorice Pizza', genre: 'Comedy', release_date: '2021-11-26 00:00:00', description: 'The story of Alana Kane and Gary Valentine growing up, running around and going through the treacherous navigation of first love in the San Fernando Valley, 1973.')
+# Movie.create(title: 'Spider-Man: Into the Spider-Verse', genre: 'Action', release_date: '2018-12-01 00:00:00', description: 'Teen Miles Morales becomes the Spider-Man of his universe, and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.')
+# Movie.create(title: 'The Good, the Bad and the Ugly', genre: 'Western', release_date: '1966-12-23 00:00:00', description: 'A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.')
+# Review.create(rating: 10, body: 'Profound', author_id: 1, movie_id: 1)
+# Review.create(rating: 1, body: 'Pretentious', author_id: 2, movie_id: 1)
+
+
+# Showtime.create(theater_id: 1, movie_id: 1, zip_code: '37901', time: '2022-05-01 14:05:00')
