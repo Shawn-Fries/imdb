09257b84edfc5ee554661458b3f32a475fff1100
Shawn-Fries <samfoley275@gmail.com>
2022-04-11 17:32:48 -0400

    fix images

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,31 +1,28 @@
 import React from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 // import spiderman from '/app/assets/images/spiderman.png'
-import { Carousel } from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-// import goodBad from '/app/assets/images/good_bad.jpg';
-// import goodBad from 'assets/images/good_bad';
-// import goodBad from 'assets/images/good_bad';
-
-// const history = useHistory();
-
-const images = [{ src: require('./images/licorice_pizza.jpg'), link: "/movies/1"},
-    { src: require('./images/spiderman.png'), link: "/movies/2" },
-    { src: require('./images/good_bad.jpg'), link: "/movies/3" }
-];
+// import { Carousel } from 'react-responsive-carousel'
+// import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 export default () => (
     <div className="splash">
         <h1 id="splash-logo">IMDb Clone</h1>
         {/* <Search/> */}
         <Link className="signin-link" to="/login">Sign In</Link>
-        <Link className="showtimes-link" to="/showtimes">Showtimes</Link>
-        <div className="carousel-wrapper">
-            <Carousel infiniteLoop useKeyboardArrows direction='horizontal'>
-                {images.map(image => (
-                    <img src={image.src} onChange={() => history.push(item.link)}/>
-                ))}
-            </Carousel>
-        </div>
+                <div>
+                <Link to="/movies/1">
+                        <img src={window.licoricePizzaURL} />
+                </Link>
+                </div>
+                <div>
+                    <Link to="/movies/2">
+                        <img src={window.spidermanURL} />
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/movies/3">
+                        <img src={window.goodBadURL} />
+                    </Link>
+                </div>
     </div>
 )
