29ca55965de6a9dc34977e117a261928ffd2fdc0
Shawn-Fries <samfoley275@gmail.com>
2022-07-12 22:32:32 -0400

    top gun maverick

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -19,16 +19,20 @@ export default () => (
                 <img src={window.licoricePizzaURL} />
             </Link>
         </div>
-        
         <div className='movie_image'>
                     <Link to="/movies/2">
                         <img src={window.spidermanURL} />
                     </Link>
-                </div>
+        </div>
         <div className='movie_image'>
                     <Link to="/movies/3">
                         <img src={window.goodBadURL} />
                     </Link>
-                </div>
+        </div>
+        <div className='movie_image'>
+            <Link to="/movies/4">
+                <img src={window.topGunURL} />
+            </Link>
+        </div>
     </div>
 )
