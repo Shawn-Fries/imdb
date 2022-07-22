fb88238f85c9b5015d039869e0dcf50deea9b5a3
Shawn-Fries <samfoley275@gmail.com>
2022-07-22 17:34:55 -0400

    remove for now

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+//import  { Search } from 'search'
 // import spiderman from '/app/assets/images/spiderman.png'
 // import { Carousel } from 'react-responsive-carousel'
 // import 'react-responsive-carousel/lib/styles/carousel.min.css'
