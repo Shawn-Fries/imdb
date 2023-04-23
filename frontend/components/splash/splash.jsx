3fdf569af2c278215c859d5404770b1350afe4e2
Shawn-Fries <samfoley275@gmail.com>
2023-04-22 22:44:26 -0400

    remove search for now

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Search from './search'
+import ReactSearchBox from 'react-search-box'
 // import spiderman from '/app/assets/images/spiderman.png'
 // import { Carousel } from 'react-responsive-carousel'
 // import 'react-responsive-carousel/lib/styles/carousel.min.css'
+/* data = [
+        {
+                key: 'Licorice Pizza',
+                        value: <Link to="/movies/1">
+                                </Link>
+                                    }
+] */
 
 export default () => (
     <div className="splash">
         <Link to='/'>
             <h1 id="splash-logo">MyMDb</h1>
         </Link>
-        <Search></Search>
+        {/* <ReactSearchBox
+            placeholder=''
+            value=''
+            data={this.data}
+        
+        /> */}
         <Link className="signin-link" to="/login">Sign In</Link>
         <div className="showtimes-link">
             <Link to="/showtimes">Showtimes near you</Link>
