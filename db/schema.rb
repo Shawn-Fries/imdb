a7cbeef0622f91556f9f6dfbe3d2f5d6f4a72d3b
Shawn-Fries <samfoley275@gmail.com>
2023-05-04 13:47:01 -0400

    update rails

--- db/schema.rb
+++ db/schema.rb
@@ -2,26 +2,25 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_195110) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_174519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
     t.string "title", null: false
     t.string "genre", null: false
-    t.datetime "release_date", null: false
+    t.datetime "release_date", precision: nil, null: false
     t.text "description", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "image_url"
     t.index ["genre"], name: "index_movies_on_genre"
     t.index ["title"], name: "index_movies_on_title"
@@ -32,8 +31,8 @@ ActiveRecord::Schema.define(version: 2022_03_28_195110) do
     t.text "body", null: false
     t.integer "author_id", null: false
     t.integer "movie_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["author_id"], name: "index_reviews_on_author_id"
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
     t.index ["rating"], name: "index_reviews_on_rating"
@@ -43,17 +42,17 @@ ActiveRecord::Schema.define(version: 2022_03_28_195110) do
     t.integer "theater_id", null: false
     t.integer "movie_id", null: false
     t.string "zip_code", null: false
-    t.datetime "time", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "time", precision: nil, null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "session_token"
     t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["session_token"], name: "index_users_on_session_token"
     t.index ["username"], name: "index_users_on_username"
   end
